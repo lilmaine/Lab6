8c2a824839dece61fe0e91eb7d4beb78e6fe71d7
tramaine <lilmaine@gmail.com>
2013-11-19 18:27:46 -0700

    updated

--- robot.h
+++ robot.h
@@ -8,6 +8,14 @@
 #ifndef ROBOT_H_
 #define ROBOT_H_
 
+#define FORWARD 1
+#define BACK 2
+#define SMALLLEFT 3
+#define LARGELEFT 4
+#define SMALLRIGHT 5
+#define LARGERIGHT 6
 
+void initiateRobot();
+void moveRobot(unsigned char direction);
 
 #endif /* ROBOT_H_ */
