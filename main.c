93940105dc394cf5ac33015dfd4bfd9926bd6268
tramaine <lilmaine@gmail.com>
2013-11-19 18:27:17 -0700

    mostly working

--- main.c
+++ main.c
@@ -7,7 +7,29 @@ int main(void)
 {
 	initiateRobot();
 	while(1){
-		moveRobot(RIGHT);
+//		moveRobot(RIGHT);
+//		__delay_cycles(1000000);
+//		moveRobot(LEFT);
+//		__delay_cycles(1000000);
+//		moveRobot(BACK);
+		moveRobot(FORWARD);
+		__delay_cycles(1000000);
+		//moveRobot(BACK);
+		//moveRobot(FORWARD);
+		//__delay_cycles(1000000);
+		moveRobot(SMALLLEFT);
+		//moveRobot(FORWARD);
+		__delay_cycles(1000000);
+		moveRobot(SMALLRIGHT);
+		//moveRobot(FORWARD);
+		__delay_cycles(1000000);
+		moveRobot(LARGELEFT);
+		//moveRobot(FORWARD);
+		__delay_cycles(1000000);
+		moveRobot(LARGERIGHT);
+		__delay_cycles(1000000);
+
+
 	}
 }
 
