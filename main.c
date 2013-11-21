50b80a975818990f66a56461f58931773fb0a4d0
tramaine <lilmaine@gmail.com>
2013-11-21 15:21:12 -0700

    required functionality

--- main.c
+++ main.c
@@ -1,35 +1,43 @@
 #include <msp430.h>
+
+/*
+ * main.c
+ */
+#include <msp430.h>
 #include "robot.h"
-
-
 
 int main(void)
 {
-	initiateRobot();
-	while(1){
-//		moveRobot(RIGHT);
-//		__delay_cycles(1000000);
-//		moveRobot(LEFT);
-//		__delay_cycles(1000000);
-//		moveRobot(BACK);
-		moveRobot(FORWARD);
-		__delay_cycles(1000000);
-		//moveRobot(BACK);
-		//moveRobot(FORWARD);
-		//__delay_cycles(1000000);
-		moveRobot(SMALLLEFT);
-		//moveRobot(FORWARD);
-		__delay_cycles(1000000);
-		moveRobot(SMALLRIGHT);
-		//moveRobot(FORWARD);
-		__delay_cycles(1000000);
-		moveRobot(LARGELEFT);
-		//moveRobot(FORWARD);
-		__delay_cycles(1000000);
-		moveRobot(LARGERIGHT);
-		__delay_cycles(1000000);
+    WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
 
+    initiateRobot();
 
-	}
+    while(1){
+    moveForward();
+    _delay_cycles(1000000);
+    stop();
+    _delay_cycles(1000000);
+    moveBack();
+    _delay_cycles(1000000);
+    stop();
+    _delay_cycles(1000000);
+    turnLeft();
+    _delay_cycles(300000);
+    stop();
+    _delay_cycles(1000000);
+    turnRight();
+    _delay_cycles(300000);
+    stop();
+    _delay_cycles(1000000);
+    turnLeft();
+    _delay_cycles(800000);
+    stop();
+    _delay_cycles(1000000);
+    turnRight();
+    _delay_cycles(800000);
+    stop();
+    _delay_cycles(1000000);
+    }
+
+    return 0;
 }
-
