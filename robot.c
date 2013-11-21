4fb0d7d9b6c05e6f7ee0f963db006b9a2aa23324
tramaine <lilmaine@gmail.com>
2013-11-21 15:21:31 -0700

    required functionality

--- robot.c
+++ robot.c
@@ -1,148 +1,109 @@
-#include <msp430.h>
 #include "robot.h"
-/*
- * robot.c
- *
- *  Created on: Nov 19, 2013
- *      Author: C15Tramaine.Barnett
- */
 
-void initiateRobot() {
-	WDTCTL = WDTPW | WDTHOLD;                 // stop the watchdog timer
+void initiateRobot(){
+    P1DIR |= BIT2;
+    P1SEL |= BIT2;
 
-	P1DIR |= BIT2;                // TA0CCR1 on P1.2
-	P1DIR |= BIT1;                // TA0CCR1 on P1.2
-	P2DIR |= BIT1;                // TA0CCR1 on P1.2
-	P2DIR |= BIT0;                // TA0CCR1 on P1.2
+    P1DIR |= BIT1;
+    P1SEL |= BIT1;
 
-	P1SEL |= BIT2;                // TA0CCR1 on P1.2
-	P1SEL |= BIT1;                // TA0CCR1 on P1.2
-	P2SEL |= BIT1;                // TA0CCR1 on P1.2
-	P2SEL |= BIT0;                // TA0CCR1 on P1.2
+    P2DIR |= BIT0;
+    P2SEL |= BIT0;
 
-	TA0CTL &= ~MC1 | MC0;            // stop timer A0
-	TA1CTL &= ~MC1 | MC0;            // stop timer A0
+    P2DIR |= BIT1;
+    P2SEL |= BIT1;
 
-	TA0CTL |= TACLR;                // clear timer A0
-	TA1CTL |= TACLR;                // clear timer A0
 
-	TA0CTL |= TASSEL1;           // configure for SMCLK
-	TA1CTL |= TASSEL1;           // configure for SMCLK
 
-	TA0CCR0 = 100;  // set signal period to 100 clock cycles (~100 microseconds)
-	TA0CCR1 = 60;                // set duty cycle to 25/100 (25%)
+        TA0CTL &= ~(MC1|MC0);            // stop timer A0
+        TA1CTL &= ~(MC1|MC0);            // stop timer A0
 
-	TA1CCR0 = 100;  // set signal period to 100 clock cycles (~100 microseconds)
-	TA1CCR1 = 60;                // set duty cycle to 25/100 (25%)
+        TA0CTL |= TACLR;                // clear timer A0
+        TA1CTL |= TACLR;
 
-	TA0CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-	TA0CCTL0 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+        TA0CTL |= TASSEL1;           // configure for SMCLK
+        TA1CTL |= TASSEL1;
 
-	TA1CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-	TA1CCTL0 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
+        TA0CCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
+        TA0CCR1 = 50;
 
-	TA0CTL |= MC0;                // count up
-	TA1CTL |= MC0;				  //count up
+        TA1CCR0 = 100;
+        TA1CCR1 = 50;
 
-//	while (1) {
-//		__delay_cycles(1000000);
-//		TACCR1 = 50;            // set duty cycle to 50/100 (50%)
-//		__delay_cycles(1000000);
-//		TACCR1 = 60;            // set duty cycle to 75/100 (75%)
-//		__delay_cycles(1000000);
-//		TACCR1 = 25;            // set duty cycle to 25/100 (25%)
-//	}
+        TA0CCTL0 |= OUTMOD2|OUTMOD0;
+        TA0CCTL1 |= OUTMOD2|OUTMOD0;
+
+        TA1CCTL0 |= OUTMOD2|OUTMOD0;
+        TA1CCTL1 |= OUTMOD2|OUTMOD0;
+
+        TA0CTL |= MC0;                // count up
+        TA1CTL |= MC0;
 
 }
 
-void moveRobot(unsigned char direction) {
-    TA0CCTL1 &= ~OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-    TA0CCTL0 &= ~OUTMOD_7;                        //set to Reset
-    TA1CCTL1 &= ~OUTMOD_7;
-    TA1CCTL0 &= ~OUTMOD_7;
-
-	switch (direction) {
-	case LARGERIGHT:
-		TA0CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-		TA0CCTL0 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-
-		TA1CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-		TA1CCTL0 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-
-		__delay_cycles(500000);
-
-		moveRobot(FORWARD);
-
-		break;
-
-	case SMALLRIGHT:
-		TA0CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-		TA0CCTL0 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-
-		TA1CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-		TA1CCTL0 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-
-		__delay_cycles(200000);
-
-		moveRobot(FORWARD);
-
-		break;
-
-	case LARGELEFT:
-
-		TA0CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-		TA0CCTL0 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-
-		TA1CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-		TA1CCTL0 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-
-		__delay_cycles(500000);
-
-		moveRobot(FORWARD);
-
-		break;
-
-	case SMALLLEFT:
-
-		TA0CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-		TA0CCTL0 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-
-		TA1CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-		TA1CCTL0 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-
-		__delay_cycles(200000);
-
-		moveRobot(FORWARD);
-
-		break;
-
-	case FORWARD:
-
-		TA0CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-		TA0CCTL0 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-
-		TA1CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-		TA1CCTL0 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-		break;
-
-	case BACK:
-
-
-		TA0CCTL1 |= OUTMOD_5;        // set TA0CCTL1 to Reset mode
-		TA0CCTL0 |= OUTMOD_7;        // set TA0CCTL1 to Reset / Set mode
-
-		TA1CCTL1 |= OUTMOD_5;        // set TA1CCTL1 to Reset mode
-		TA1CCTL0 |= OUTMOD_7;        // set TA1CCTL1 to Reset / Set mode
-		break;
-
-
-	case STOP:
-
-        TA0CCTL1 |= OUTMOD_5;          // set TA0CCTL1 to Reset / Set mode
-        TA0CCTL0 |= OUTMOD_5;                   // set TA0CCTL0 to ground
-
-        TA1CCTL1 |= OUTMOD_5;          // set TA1CCTL1 to Reset / Set mode
-        TA1CCTL0 |= OUTMOD_5;
-        break;
-	}
+void leftMotorBackward(){
+        TA0CCTL1 &= ~OUTMOD1;
+        TA0CCTL1 |= OUTMOD2|OUTMOD0;
+        _delay_cycles(20000);
+        TA0CCTL0 &= ~OUTMOD0;
+        TA0CCTL0 |= OUTMOD2;
 }
+
+void moveLeftMotorForward(){
+        TA0CCTL0 &= ~OUTMOD1;
+        TA0CCTL0 |= OUTMOD2|OUTMOD0;
+        _delay_cycles(20000);
+        TA0CCTL1 &= ~OUTMOD0;
+        TA0CCTL1 |= OUTMOD2;
+}
+
+void rightMotorBackward(){
+        TA1CCTL1 &= ~OUTMOD1;
+        TA1CCTL1 |= OUTMOD2|OUTMOD0;
+        _delay_cycles(20000);
+        TA1CCTL0 &= ~OUTMOD0;
+        TA1CCTL0 |= OUTMOD2;
+
+}
+
+void moveLeftMotorForward(){
+        TA1CCTL0 &= ~OUTMOD1;
+        TA1CCTL0 |= OUTMOD2|OUTMOD0;
+        _delay_cycles(20000);
+        TA1CCTL1 &= ~OUTMOD0;
+        TA1CCTL1 |= OUTMOD2;
+}
+
+void moveForward(){
+        moveLeftMotorForward();
+        moveLeftMotorForward();
+}
+
+void moveBack(){
+        leftMotorBackward();
+        rightMotorBackward();
+}
+
+void turnLeft(){
+        leftMotorBackward();
+        moveLeftMotorForward();
+}
+
+void turnRight(){
+        moveLeftMotorForward();
+        rightMotorBackward();
+}
+
+void stop(){
+
+    TA0CCTL1 &= ~(OUTMOD_7);        // set TACCTL1 to Reset / Set mode
+    TA0CCTL1 |= OUTMOD_5;          // set TA0CCTL1 to Reset / Set mode
+    TA0CCTL0 &= ~(OUTMOD_7);                        //set to Reset
+    TA0CCTL0 |= OUTMOD_5;                   // set TA0CCTL0 to ground
+    TA1CCTL1 &= ~(OUTMOD_7);
+    TA1CCTL1 |= OUTMOD_5;          // set TA1CCTL1 to Reset / Set mode
+    TA1CCTL0 &= ~(OUTMOD_7);
+    TA1CCTL0 |= OUTMOD_5;
+}
+
+
